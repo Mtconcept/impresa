ec8f00d910fef1f4f59c799d0d7e756718b35e51
Okanlawon Jamiu <developerjamiu@gmail.com>
2020-08-29 16:50:57 +0100

    Fixed Font Imports in pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
+      home: Scaffold(),
     );
   }
 }
