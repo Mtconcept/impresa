cea3d887457b456f21f831df19e557e124690920
Okanlawon Jamiu <developerjamiu@gmail.com>
2020-08-30 23:19:39 +0100

    Added a Form Header Custom Widget

---
+++ lib/ui/widgets/form_header.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class FormHeader extends StatelessWidget {
+  final String title;
+  final String description;
+
+  const FormHeader({
+    Key key,
+    @required this.title,
+    @required this.description,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width * 0.6,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              SizedBox(height: 10),
+              Text(
+                description,
+                style: Theme.of(context).textTheme.bodyText2,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
