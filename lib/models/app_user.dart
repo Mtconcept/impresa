ec5de77162039758c88b4a1ad33fc7612e3dcddb
Okanlawon Jamiu <developerjamiu@gmail.com>
2020-09-15 01:19:34 +0100

    Added a App User Model

---
+++ lib/models/app_user.dart
@@ -0,0 +1,13 @@
+class AppUser {
+  final String id;
+  final String fullName;
+  final String emailAddress;
+  final String phoneNumber;
+
+  AppUser({
+    this.id,
+    this.fullName,
+    this.emailAddress,
+    this.phoneNumber,
+  });
+}
