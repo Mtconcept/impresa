f8c48411ecbafb6e5af62b8c461449105df088ce
Okanlawon Jamiu <developerjamiu@gmail.com>
2020-09-15 15:28:09 +0100

    Updated Login Controller

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -2,8 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:impresa/views/screens/home.dart';
+
 import '../core/utils/validation_mixin.dart';
+import '../views/screens/home_screen.dart';
 import '../views/screens/register_screen.dart';
 
 class LoginController extends GetxController with ValidationMixin {
@@ -32,7 +33,7 @@ class LoginController extends GetxController with ValidationMixin {
     user =
         await auth.signInWithEmailAndPassword(email: email, password: password);
     if (user != null) {
-      Get.to(Home());
+      Get.to(HomeScreen());
     }
   }
 }
